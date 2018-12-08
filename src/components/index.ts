cdd29da3f08c7b18d521f9276aff1036a214080f
adamjking3 <adamjking3@gmail.com>
2018-12-07 21:45:15 -0700

    Rename HeaderPanel -> ActionPanel

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,7 +1,7 @@
 import SidePanel from "./SidePanel";
 import ToolPanel from "./ToolPanel";
-import HeaderPanel from "./HeaderPanel";
+import ActionPanel from "./ActionPanel";
 import DrawingCanvas from "./DrawingCanvas";
 import HotKeyListener from "./HotKeyListener";
 
-export { SidePanel, ToolPanel, HeaderPanel, DrawingCanvas, HotKeyListener };
+export { SidePanel, ToolPanel, ActionPanel, DrawingCanvas, HotKeyListener };
